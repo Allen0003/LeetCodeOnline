cdc234efe276c18279783758ed9f4583787ea947
Allen0003 <apss1943@gmail.com>
2019-12-13 16:40:58 +0800

    Update L020_E.java

--- One2Twenty/L020_E.java
+++ One2Twenty/L020_E.java
@@ -7,6 +7,7 @@
 //        Open brackets must be closed in the correct order.
 //        Note that an empty string is also considered valid.
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class L020_E {
@@ -38,5 +39,45 @@ public class L020_E {
             }
         }
         return expectStack.isEmpty();
+    }
+
+
+    private HashMap<Character, Character> mappings;
+
+    // Initialize hash map with mappings. This simply makes the code easier to read.
+    public L020_E() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    //official solution
+    public boolean isValid2(String s) {
+
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (this.mappings.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
+        }
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
     }
 }
