ed595e6914f78552de60f4f7877c78e7ad5443b0
Allen0003 <apss1943@gmail.com>
2020-02-26 16:28:06 +0800

    Add files via upload

--- L150To200.java
+++ L150To200.java
@@ -1,7 +1,9 @@
 import basicDataStructure.ListNode;
+import org.omg.PortableInterceptor.INACTIVE;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class L150To200 {
@@ -114,9 +116,80 @@ public class L150To200 {
             return "Y";
         if (n == 0)
             return "Z";
-
         return "";
     }
+
+
+    public int numberToCharacter(String n) {
+        if (n.equals("A"))
+            return 1;
+        if (n.equals("B"))
+            return 2;
+        if (n.equals("C"))
+            return 3;
+        if (n.equals("D"))
+            return 4;
+        if (n.equals("E"))
+            return 5;
+        if (n.equals("F"))
+            return 6;
+        if (n.equals("G"))
+            return 7;
+        if (n.equals("H"))
+            return 8;
+        if (n.equals("I"))
+            return 9;
+        if (n.equals("J"))
+            return 10;
+        if (n.equals("K"))
+            return 11;
+        if (n.equals("L"))
+            return 12;
+        if (n.equals("M"))
+            return 13;
+        if (n.equals("N"))
+            return 14;
+        if (n.equals("O"))
+            return 15;
+        if (n.equals("P"))
+            return 16;
+        if (n.equals("Q"))
+            return 17;
+        if (n.equals("R"))
+            return 18;
+        if (n.equals("S"))
+            return 19;
+        if (n.equals("T"))
+            return 20;
+        if (n.equals("U"))
+            return 21;
+        if (n.equals("V"))
+            return 22;
+        if (n.equals("W"))
+            return 23;
+        if (n.equals("X"))
+            return 24;
+        if (n.equals("Y"))
+            return 25;
+        if (n.equals("Z"))
+            return 26;
+        return 0;
+    }
+
+    public int titleToNumber(String s) {
+        int result = 0;
+        if (s.equals("")) {
+            return result;
+        }
+        String target[] = s.split("");
+        int digitalIndex = 0;
+        for (int i = target.length - 1; i >= 0; i--) {
+            result += this.numberToCharacter(target[i]) * (Math.pow(26, digitalIndex));
+            digitalIndex = digitalIndex + 1;
+        }
+        return result;
+    }
+
 
     public String convertToTitle(int n) {
         if (n < 1) {
@@ -139,5 +212,14 @@ public class L150To200 {
         return result;
     }
 
+    public int trailingZeroes(int n) {
+        int countZero = 0;
+        while (n != 0) {
+            n = n / 5;
+            countZero += n;
+
+        }
+        return countZero;
+    }
 
 }
