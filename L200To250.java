b54588bf82163f0e310f1369f80d16a437fadbad
Allen0003 <apss1943@gmail.com>
2020-03-18 16:42:15 +0800

    Add files via upload

--- L200To250.java
+++ L200To250.java
@@ -1,8 +1,10 @@
 import basicDataStructure.ListNode;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class L200To250 {
 
@@ -88,8 +90,6 @@ public class L200To250 {
         }
     }
 
-    // 1. divide the list to two parts
-    // 2. reverse the second part
 
     public boolean isPalindrome(ListNode head) {
 
@@ -121,6 +121,57 @@ public class L200To250 {
                     break;
                 }
             }
+        }
+        return result;
+    }
+
+
+    public List<Integer> majorityElement(int[] nums) {
+
+        if (nums.length < 3) {
+            return Arrays.stream(nums).distinct().boxed().collect(Collectors.toList());
+        }
+
+        List<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> count = new HashMap<>();
+
+        for (int num : nums) {
+            if (count.get(num) == null) {
+                count.put(num, 1);
+            } else {
+                count.put(num, count.get(num) + 1);
+                if (count.get(num) > nums.length / 3) {
+                    result.add(num);
+                }
+            }
+        }
+
+        return result.stream().distinct().collect(Collectors.toList());
+    }
+
+
+    public List<String> summaryRanges(int[] nums) {
+        if (nums.length == 0){
+            return new ArrayList<>();
+        }
+
+        List<String> result = new ArrayList<>();
+        int start = nums[0];
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] + 1 != nums[i + 1]) { // find the end
+                if (start == nums[i]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(String.valueOf(start) + "->" + String.valueOf(nums[i]));
+                }
+                start = nums[i + 1];
+            }
+        }
+
+        if (start != nums[nums.length - 1]) {
+            result.add(String.valueOf(start) + "->" + String.valueOf(nums[nums.length - 1]));
+        } else {
+            result.add(String.valueOf(start));
         }
         return result;
     }
