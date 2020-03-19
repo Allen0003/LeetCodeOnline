ce48b0cfdbc026cf08e52279320c2982682a2901
Allen0003 <apss1943@gmail.com>
2020-03-19 14:24:44 +0800

    Add files via upload

---
+++ Sql/Duplicate Emails.sql
@@ -0,0 +1,36 @@
+
+
+
+Q: Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+
+
+
+A: 
+select Email from Person
+Group by Email
+HAVING count(Email) > 1
+
+
+
+
+
+
+
+
+
+
---
+++ Sql/Second Highest Salary.sql
@@ -0,0 +1,29 @@
+
+
+
+
+Q: Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+
+
+A: SELECT MAX(Salary) as secondHighestSalary FROM employee 
+where secondHighestSalary < (select MAX(Salary) from employee)
+
+
+
