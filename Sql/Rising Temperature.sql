8f81d5759989faf39dab90012a536173ec790612
Allen0003 <apss1943@gmail.com>
2020-03-19 15:05:35 +0800

    Add files via upload

---
+++ Sql/Rising Temperature.sql
@@ -0,0 +1,38 @@
+
+
+Q: Given a Weather table, write a SQL query to find all dates' 
+Ids with higher temperature compared to its previous (yesterday's) dates.
+
++---------+------------------+------------------+
+| Id(INT) | RecordDate(DATE) | Temperature(INT) |
++---------+------------------+------------------+
+|       1 |       2015-01-01 |               10 |
+|       2 |       2015-01-02 |               25 |
+|       3 |       2015-01-03 |               20 |
+|       4 |       2015-01-04 |               30 |
++---------+------------------+------------------+
+For example, return the following Ids for the above Weather table:
+
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+
+
+
+
+A: 
+
+select w.id 
+from Weather w, Weather w2 
+where w.RecordDate-1 = w2.RecordDate and w.Temperature > w2.temperature
+
+
+
+
+
+
+
+
+
