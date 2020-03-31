5655ad109b9e610d5c44734b9209b2eca387d99b
Allen0003 <apss1943@gmail.com>
2020-03-31 17:08:11 +0800

    Add files via upload

--- test/L250_300_Test.java
+++ test/L250_300_Test.java
@@ -7,9 +7,30 @@ public class L250_300_Test {
 
     L250To300 forTest = new L250To300();
 
-//    @Test
-//    public void testRemoveElements1() {
-//        assertEquals(forTest.removeElements(a, 2), a);
-//    }
+    @Test
+    public void testGetCase1() {
+        assertEquals(forTest.getHint("1807", "7810"), "1A3B");
+    }
+
+    @Test
+    public void testGetCase2() {
+        assertEquals(forTest.getHint("1123", "0111"), "1A1B");
+    }
+
+    @Test
+    public void testGetCase3() {
+        assertEquals(forTest.getHint("1122", "1222"), "3A0B");
+    }
+
+
+    @Test
+    public void testCanWinNimCase1() {
+        assertEquals(forTest.canWinNim(4), false);
+    }
+
+    @Test
+    public void testCanWinNimCase2() {
+        assertEquals(forTest.canWinNim(5), true);
+    }
 
 }
