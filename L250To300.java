630fd24f9037a32f0ad5144798c771b3f9e52350
Allen0003 <apss1943@gmail.com>
2020-04-09 15:44:48 +0800

    Add files via upload

--- L250To300.java
+++ L250To300.java
@@ -1,3 +1,5 @@
+import basicDataStructure.TreeNode;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -61,8 +63,80 @@ public class L250To300 {
         } else if (num % 5 == 0) {
             return isUgly(num / 5);
         }
-
         return false;
     }
+
+
+    public int addDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += (num % 10);
+            num = num / 10;
+            if (num <= 0 && sum >= 10) {
+                num = sum;
+                sum = 0;
+            }
+        }
+        return sum;
+    }
+
+
+    public List<String> binaryTreePaths(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        List<String> result = new ArrayList<>();
+
+        Stack<TreeNode> nodeStack = new Stack<>();
+        nodeStack.push(root);
+
+        HashMap<TreeNode, TreeNode> parent = new HashMap<>();
+        parent.put(root, null);
+
+        while (!nodeStack.isEmpty()) {
+            TreeNode current = nodeStack.pop();
+            if (current.left == null && current.right == null) {
+                Stack<TreeNode> stk = new Stack<>();
+                while (current != null) {
+                    stk.push(current);
+                    current = parent.get(current);
+                }
+                String temp = "";
+                while (!stk.isEmpty()) {
+                    current = stk.pop();
+                    temp += current.val + "->";
+                }
+                result.add(temp.length() != 0 ? temp.substring(0, temp.length() - 2) : "");
+            }
+            if (current.right != null) {
+                parent.put(current.right, current);
+                nodeStack.push(current.right);
+            }
+            if (current.left != null) {
+                parent.put(current.left, current);
+                nodeStack.push(current.left);
+            }
+        }
+
+        return result;
+    }
+
+    public int[] singleNumber(int[] nums) {
+
+        if (nums.length == 0) {
+            return new int[]{0, 0};
+        }
+
+        List<Integer> arrayList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+
+        ArrayList<Integer> temp = new ArrayList<>(arrayList);
+        for (Integer number : new HashSet<Integer>(arrayList)) {
+            temp.remove(number);
+        }
+        arrayList.removeAll(temp);
+        return arrayList.stream().mapToInt(i -> i).toArray();
+    }
+
 
 }
